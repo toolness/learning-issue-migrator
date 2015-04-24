bc10a5be1b5fa73c10db35c1070715a64391c939
Atul Varma <varmaa@gmail.com>
2015-04-24 18:52:05 -0400

    cache issues

--- cache-issues.js
+++ cache-issues.js
@@ -21,10 +21,11 @@ var issueStream = new PagedEntityStream({
 }).pipe(through(function ignoreIssueIfAlreadyCached(issue) {
   if (!fs.existsSync(issueFilename(issue))) {
     this.queue(issue);
+  } else {
+    console.log('Skipping issue ' + issue.number + ' (already cached).');
   }
 })).pipe(through(function addCommentsToIssue(issue) {
   var self = this;
-
   var allComments = [];
   var comments = new PagedEntityStream({
     userAgent: USER_AGENT,
@@ -32,19 +33,16 @@ var issueStream = new PagedEntityStream({
          '/issues/' + issue.number + '/comments',
     user: GITHUB_USERNAME,
     pass: GITHUB_PASSWORD
-  });
-
-  comments.on('data', function(comment) {
+  }).on('data', function(comment) {
     allComments.push(comment);
-  });
-  comments.on('end', function() {
+  }).on('end', function() {
     issue.comments = allComments;
     self.emit('data', issue);
     self.resume();
   });
   self.pause();
 })).on('data', function cacheIssue(issue) {
-  console.log('caching issue ' + issue.number);
+  console.log('Caching issue ' + issue.number + '.');
   fs.writeFileSync(issueFilename(issue), JSON.stringify(issue, null, 2));
 });
 

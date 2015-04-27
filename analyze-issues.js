c4fb786eebd9876c2b61f723b3b39ea22bb0e1e5
Atul Varma <varmaa@gmail.com>
2015-04-27 09:01:24 -0400

    analyze-issues.js supports multiple commands.

--- analyze-issues.js
+++ analyze-issues.js
@@ -1,7 +1,10 @@
+var path = require('path');
 var _ =  require('underscore');
 var chalk = require('chalk');
 
 var cacheIssues = require('./cache-issues');
+
+var commands = {};
 
 function scanIssue(issue, regex) {
   var matches = [];
@@ -47,17 +50,44 @@ function isNonPlatformIssue(issue) {
   return !isPlatformIssue(issue);
 }
 
-cacheIssues.getAllCachedIssues()
-  .filter(isNonPlatformIssue)
-  .forEach(function(issue) {
-    var linkedIssues = findLinkedIssues(issue);
-
-    if (linkedIssues.length) {
-      console.log(chalk.white.bold('#' + issue.number),
-                  chalk.gray('(' + issue.title + ')'),
-                  "links to issue(s)", 
-                  linkedIssues.map(function(i) {
-                    return chalk.cyan(i);
-                  }).join(', ') + ".");
-    }
+function help(exitcode) {
+  console.log("usage: " + path.basename(process.argv[1]) + ' [command]');
+  console.log("\ncommands:\n");
+  Object.keys(commands).forEach(function(name) {
+    console.log('  ' + name, '-', commands[name].help);
   });
+  process.exit(exitcode || 0);
+}
+
+function main() {
+  var arg = process.argv[2];
+  var cmd = commands[arg];
+
+  if (['-h', '--help'].indexOf(arg) != -1) return help();
+  if (!cmd) return help(1);
+  cmd.run();
+}
+
+commands['linked'] = {
+  help: 'show non-platform issues linking to other issues',
+  run: function() {
+    cacheIssues.getAllCachedIssues()
+      .filter(isNonPlatformIssue)
+      .forEach(function(issue) {
+        var linkedIssues = findLinkedIssues(issue);
+
+        if (linkedIssues.length) {
+          console.log(chalk.white.bold('#' + issue.number),
+                      chalk.gray('(' + issue.title + ')'),
+                      "links to issue(s)",
+                      linkedIssues.map(function(i) {
+                        return chalk.cyan(i);
+                      }).join(', ') + ".");
+        }
+      });
+  }
+};
+
+if (!module.parent) {
+  main();
+}
